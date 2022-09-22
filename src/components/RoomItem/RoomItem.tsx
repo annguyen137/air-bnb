53c2d129060a864fc549ff52bc2399edd9d93736
annguyen13796 <nguyenlaman.1307@gmail.com>
2022-09-23 02:56:00 +0700

    fix css aspect ratio for unsupport browser

--- src/components/RoomItem/RoomItem.tsx
+++ src/components/RoomItem/RoomItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Stack, Button } from "@mui/material";
+import { Box, Stack, Button, Typography } from "@mui/material";
 import { Room } from "interfaces/room";
 import { NavLink, useNavigate } from "react-router-dom";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
@@ -18,7 +18,7 @@ const RoomItem = ({ room }: Props) => {
 
   return (
     <Box className={`${styles["room-card"]}`}>
-      <Box>
+      <Stack sx={{ height: "100%" }}>
         <Box className={`${styles["room-img"]}`}>
           <NavLink to={`/rooms/${room._id}`} target={"_blank"}>
             <img src={`${room.image}`} alt={room.name} />
@@ -97,15 +97,18 @@ const RoomItem = ({ room }: Props) => {
             </Box>
           </Box>
         </Box>
-        <NavLink to={`/rooms/${room._id}`} target={"_blank"}>
-          <Box>
+        <NavLink to={`/rooms/${room._id}`} target={"_blank"} style={{ flexGrow: 1 }}>
+          <Stack justifyContent={"space-between"} sx={{ height: "100%" }}>
             <h4 className={`${styles["room-name"]}`}>{room.name}</h4>
+            <Typography variant="body2">
+              {room.locationId?.name}, {room.locationId?.province}, {room.locationId?.country}
+            </Typography>
             <p className={`${styles["room-price"]}`}>
               <span>${room.price}</span> đêm
             </p>
-          </Box>
+          </Stack>
         </NavLink>
-      </Box>
+      </Stack>
 
       <FavoriteIcon className={styles["favourite"]} />
     </Box>
