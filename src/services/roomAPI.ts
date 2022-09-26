a930fd4055cf727c5431666ad978a7654d8d4815
annguyen13796 <nguyenlaman.1307@gmail.com>
2022-09-26 18:51:27 +0700

    update book room feature

--- src/services/roomAPI.ts
+++ src/services/roomAPI.ts
@@ -1,5 +1,8 @@
 import axiosConfig from "./axiosConfig";
 import { RoomQueryParams, Room } from "interfaces/room";
+import { User } from "interfaces/user";
+import { string } from "yup";
+import { BookTicket } from "interfaces/ticket";
 
 const roomAPI = {
   getRoomListByLocation: ({ locationId, limit, skip }: RoomQueryParams) => {
@@ -7,6 +10,13 @@ const roomAPI = {
   },
   getRoomDetail: ({ roomId }: RoomQueryParams) => {
     return axiosConfig.get<unknown, Room>(`rooms/${roomId}`);
+  },
+  bookRoomById: (values: BookTicket) => {
+    return axiosConfig.post<unknown, { message: string; userDetail: User }>(`rooms/booking`, values, {
+      params: {
+        roomId: values.roomId,
+      },
+    });
   },
 };
 
